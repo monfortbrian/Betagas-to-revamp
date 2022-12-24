11ed2a566952bddfcc6273e7d70656517b07ebf8
trezz <anahimana3@gmail.com>
2022-12-24 11:02:14 +0200

    Working on i18n

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,49 +1,81 @@
 /** @jsx jsx */
-import { jsx, Container, Flex, Button } from 'theme-ui';
-import { keyframes } from '@emotion/core';
-import { Link } from 'react-scroll';
-import Logo from 'components/logo';
-import LogoDark from 'assets/betagas logo.png';
-import { DrawerProvider } from '../../contexts/drawer/drawer.provider';
-import MobileDrawer from './mobile-drawer';
-import menuItems from './header.data';
+import { jsx, Container, Flex, Button, Select } from "theme-ui";
+import { keyframes } from "@emotion/core";
+import { Link } from "react-scroll";
+import Logo from "components/logo";
+import LogoDark from "assets/betagas logo.png";
+import { DrawerProvider } from "../../contexts/drawer/drawer.provider";
+import MobileDrawer from "./mobile-drawer";
+import menuItems from "./header.data";
+import { IoIosAddCircle } from "react-icons/io";
+import t from "../../locales";
+import { useRouter } from "next/router";
 
 export default function Header({ className }) {
-  return (
-    <DrawerProvider>
-      <header sx={styles.header} className={className} id="header">
-        <Container sx={styles.container}>
-          <Logo src={LogoDark} />
+	const router = useRouter();
+	const { locale } = router;
 
-          <Flex as="nav" sx={styles.nav}>
-            {menuItems.map(({ path, label }, i) => (
-              <Link
-                activeClass="active"
-                to={path}
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                key={i}
-              >
-                {label}
-              </Link>
-            ))}
-          </Flex>
+	const handleTranslation = () => {
+		switch (locale) {
+			case "en":
+				router.push(router.pathname, router.pathname, { locale: "fr" });
+				break;
+			case "fr":
+				router.push(router.pathname, router.pathname, { locale: "en" });
+				break;
+			default:
+				break;
+		}
+	};
+	return (
+		<DrawerProvider>
+			<header sx={styles.header} className={className} id="header">
+				<Container sx={styles.container}>
+					<Logo src={LogoDark} />
 
-          <Button
-            className="donate__btn"
-            variant="secondary"
-            aria-label="Become Distributor"
-          >
-            Become Distributor
-          </Button>
-
-          <MobileDrawer />
-        </Container>
-      </header>
-    </DrawerProvider>
-  );
+					<Flex as="nav" sx={styles.nav}>
+						{menuItems.map(({ path, label }, i) => (
+							<Link
+								activeClass="active"
+								to={path}
+								spy={true}
+								smooth={true}
+								offset={-70}
+								duration={500}
+								key={i}
+							>
+								{t[locale][label]}
+							</Link>
+						))}
+					</Flex>
+					<Flex
+						sx={{
+							gap: 2,
+						}}
+					>
+						{/* <Select
+							defaultValue="Hello"
+							sx={{
+								minWidth: "60px",
+							}}
+						>
+							<option>En</option>
+							<option>Fr</option>
+						</Select> */}
+						<Button
+							className="donate__btn"
+							variant="secondary"
+							aria-label="Become Distributor"
+							onClick={handleTranslation}
+						>
+							Become Distributor
+						</Button>
+					</Flex>
+					<MobileDrawer />
+				</Container>
+			</header>
+		</DrawerProvider>
+	);
 }
 
 const positionAnim = keyframes`
@@ -60,57 +92,57 @@ const positionAnim = keyframes`
 `;
 
 const styles = {
-  header: {
-    color: 'text',
-    fontWeight: 'body',
-    py: 4,
-    width: '100%',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    backgroundColor: 'transparent',
-    transition: 'all 0.4s ease',
-    animation: `${positionAnim} 0.4s ease`,
-    '.donate__btn': {
-      flexShrink: 0,
-      mr: [15, 20, null, null, 0],
-      ml: ['auto', null, null, null, 0],
-    },
-    '&.sticky': {
-      position: 'fixed',
-      backgroundColor: 'background',
-      color: '#000000',
-      boxShadow: '0 1px 2px rgba(0, 0, 0, 0.06)',
-      py: 3,
-      'nev > a': {
-        color: 'text',
-      },
-    },
-  },
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  nav: {
-    mx: 'auto',
-    display: 'none',
-    '@media screen and (min-width: 1024px)': {
-      display: 'block',
-    },
-    a: {
-      fontSize: 2,
-      fontWeight: 'body',
-      px: 5,
-      cursor: 'pointer',
-      lineHeight: '1.2',
-      transition: 'all 0.15s',
-      '&:hover': {
-        color: 'primary',
-      },
-      '&.active': {
-        color: 'primary',
-      },
-    },
-  },
+	header: {
+		color: "text",
+		fontWeight: "body",
+		py: 4,
+		width: "100%",
+		position: "absolute",
+		top: 0,
+		left: 0,
+		backgroundColor: "transparent",
+		transition: "all 0.4s ease",
+		animation: `${positionAnim} 0.4s ease`,
+		".donate__btn": {
+			flexShrink: 0,
+			mr: [15, 20, null, null, 0],
+			ml: ["auto", null, null, null, 0],
+		},
+		"&.sticky": {
+			position: "fixed",
+			backgroundColor: "background",
+			color: "#000000",
+			boxShadow: "0 1px 2px rgba(0, 0, 0, 0.06)",
+			py: 3,
+			"nev > a": {
+				color: "text",
+			},
+		},
+	},
+	container: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "space-between",
+	},
+	nav: {
+		mx: "auto",
+		display: "none",
+		"@media screen and (min-width: 1024px)": {
+			display: "block",
+		},
+		a: {
+			fontSize: 2,
+			fontWeight: "body",
+			px: 5,
+			cursor: "pointer",
+			lineHeight: "1.2",
+			transition: "all 0.15s",
+			"&:hover": {
+				color: "primary",
+			},
+			"&.active": {
+				color: "primary",
+			},
+		},
+	},
 };
--- src/sections/key-feature.js
+++ src/sections/key-feature.js
@@ -1,83 +1,93 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { Container, Grid } from 'theme-ui';
-import SectionHeader from '../components/section-header';
-import FeatureCardColumn from 'components/feature-card-column.js';
-import Performance from 'assets/key-feature/performance.svg';
-import Partnership from 'assets/key-feature/partnership.svg';
-import Subscription from 'assets/key-feature/subscription.svg';
-import Support from 'assets/key-feature/support.svg';
-import { Text } from 'theme-ui';
+import { jsx } from "theme-ui";
+import { Container, Grid } from "theme-ui";
+import SectionHeader from "../components/section-header";
+import FeatureCardColumn from "components/feature-card-column.js";
+import Performance from "assets/key-feature/performance.svg";
+import Partnership from "assets/key-feature/partnership.svg";
+import Subscription from "assets/key-feature/subscription.svg";
+import Support from "assets/key-feature/support.svg";
+import { Text } from "theme-ui";
+import t from "../../src/locales";
+import { useRouter } from "next/router";
 
 const data = [
-  {
-    id: 1,
-    imgSrc: Performance,
-    altText: 'Our Objective',
-    title: 'Our Objective',
-    text: 'To provide the best LPG supply solutions in the most cost effective manner with limited time consumption and always equip ourselves to fulfil customers’ demand.',
-  },
-  {
-    id: 2,
-    imgSrc: Partnership,
-    altText: 'Our Objective',
-    title: 'Our Mission',
-    text: 'To contribute to the sustainable economic, environmental and health development of Burundi through the safe distribution and use of LPG.',
-  },
-  {
-    id: 3,
-    imgSrc: Subscription,
-    altText: 'Our Objective',
-    title: 'Our Vision',
-    text: 'Our vision is to be a reliable and ethical company in providing high quality service to our customers with complete eco-friendly energy solutions',
-  },
+	{
+		id: 1,
+		imgSrc: Performance,
+		altText: "Our Objective",
+		title: "Our Objective",
+		text: "To provide the best LPG supply solutions in the most cost effective manner with limited time consumption and always equip ourselves to fulfil customers’ demand.",
+	},
+	{
+		id: 2,
+		imgSrc: Partnership,
+		altText: "Our Objective",
+		title: "Our Mission",
+		text: "To contribute to the sustainable economic, environmental and health development of Burundi through the safe distribution and use of LPG.",
+	},
+	{
+		id: 3,
+		imgSrc: Subscription,
+		altText: "Our Objective",
+		title: "Our Vision",
+		text: "Our vision is to be a reliable and ethical company in providing high quality service to our customers with complete eco-friendly energy solutions",
+	},
 ];
 
 export default function KeyFeature() {
-  return (
-    <section sx={{ variant: 'section.keyFeature' }} id="feature">
-      <Container>
-        <SectionHeader slogan="About Us" title="Welcome to Betagas" />
-        <SectionHeader
-          title2="Betagas is a leading company of storage and distribution of cleaner,
-          affordable and environmentally friendly LPG since 2020."
-        />
+	const { locale } = useRouter();
 
-        <Grid sx={styles.grid}>
-          {data.map((item) => (
-            <FeatureCardColumn
-              key={item.id}
-              src={item.imgSrc}
-              alt={item.altText}
-              title={item.title}
-              text={item.text}
-            />
-          ))}
-        </Grid>
-      </Container>
-    </section>
-  );
+	return (
+		<section sx={{ variant: "section.keyFeature" }} id="feature">
+			<Container>
+				<SectionHeader
+					slogan={t[locale]["About Us"]}
+					title={t[locale]["Welcome to Betagas"]}
+				/>
+				<SectionHeader
+					title2={
+						t[locale][
+							"Betagas is a leading company of storage and distribution of cleaner, affordable and environmentally friendly LPG since 2020."
+						]
+					}
+				/>
+
+				<Grid sx={styles.grid}>
+					{data.map(item => (
+						<FeatureCardColumn
+							key={item.id}
+							src={item.imgSrc}
+							alt={item.altText}
+							title={t[locale][item.title]}
+							text={t[locale][item.text]}
+						/>
+					))}
+				</Grid>
+			</Container>
+		</section>
+	);
 }
 
 const styles = {
-  grid: {
-    width: ['100%', '80%', '100%'],
-    mx: 'auto',
-    gridGap: [
-      '35px 0',
-      null,
-      '40px 40px',
-      '50px 60px',
-      '30px',
-      '50px 40px',
-      '55px 90px',
-    ],
-    gridTemplateColumns: [
-      'repeat(1,1fr)',
-      null,
-      'repeat(2,1fr)',
-      null,
-      'repeat(3,1fr)',
-    ],
-  },
+	grid: {
+		width: ["100%", "80%", "100%"],
+		mx: "auto",
+		gridGap: [
+			"35px 0",
+			null,
+			"40px 40px",
+			"50px 60px",
+			"30px",
+			"50px 40px",
+			"55px 90px",
+		],
+		gridTemplateColumns: [
+			"repeat(1,1fr)",
+			null,
+			"repeat(2,1fr)",
+			null,
+			"repeat(3,1fr)",
+		],
+	},
 };
